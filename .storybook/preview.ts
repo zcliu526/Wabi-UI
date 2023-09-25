a4bdc8c612c3162a79746f2f3e3704f5561feee4
zcliu526 <lzc967195@gmail.com>
2023-09-24 20:24:56 -0500

    Added more components

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,4 @@
 import type { Preview } from "@storybook/react";
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import "../src/styles/index.scss"
